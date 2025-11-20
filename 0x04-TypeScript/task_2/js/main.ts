b1c2008df211783fcf9b3a748308c36cf5f27156
Billyrono <billyrono46@gmail.com>
2025-11-20 20:29:05 +0300

    Add teachClass with string literal

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,7 +57,7 @@ console.log(employee3 instanceof Teacher ? "Teacher" : "Director");
 
 type Subjects = 'Math' | 'History';
 
-export function teachClass(todayClass: Subjects): string {
+function teachClass(todayClass: Subjects): string {
   if (todayClass === 'Math') {
     return 'Teaching Math';
   } else if (todayClass === 'History') {
