9184108ba38697153ec9c17034741a79683f4730
Billyrono <billyrono46@gmail.com>
2025-11-20 17:59:17 +0300

    Add Task_2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,56 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+const employee1 = createEmployee(200);
+console.log(employee1 instanceof Teacher ? "Teacher" : "Director");
+
+const employee2 = createEmployee(1000);
+console.log(employee2 instanceof Teacher ? "Teacher" : "Director");
+
+const employee3 = createEmployee("$500");
+console.log(employee3 instanceof Teacher ? "Teacher" : "Director");
